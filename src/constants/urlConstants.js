3d1ca80206594596ddc16cc62622eb8fdadb5bfd
vaishnaviu <vaishnaviu96@gmail.com>
2023-12-01 09:11:17 -0800

    Updated backend URL

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -1,2 +1,2 @@
-//export const SAR_BACKEND_URL = "http://localhost:5001"
-export const SAR_BACKEND_URL = "http://54.193.92.48:5001"
+export const SAR_BACKEND_URL = "http://localhost:5001"
+// export const SAR_BACKEND_URL = "http://54.193.92.48:5001"
