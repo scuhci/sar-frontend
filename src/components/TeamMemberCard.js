45f4a1d1550319e665f44c4f431f8ad896756dde
Akaash Trivedi <akaash.t1@gmail.com>
2023-11-30 23:13:13 -0800

    Team members card and info

---
+++ src/components/TeamMemberCard.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Card, CardContent, Typography, Avatar, Link, Grid } from '@mui/material';
+import "../css/TeamMemberCard.css"
+
+const TeamMemberCard = ({ name, role, imageUrl, linkedInUrl }) => {
+  return (
+    <Grid item xs={12} sm={4} md={4}>
+      <Card raised>
+        <CardContent className="team-card-content">
+            <Avatar src={imageUrl} alt={name} className="team-avatar" />
+            <div className="team-info">
+            <Typography variant="h6" className="team-name">{name}</Typography>
+            <Typography variant="subtitle1" className="team-role">{role}</Typography>
+            <Link href={linkedInUrl} target="_blank" rel="noopener" className="team-linkedin">
+                LinkedIn
+            </Link>
+            </div>
+        </CardContent>
+     </Card>
+    </Grid>
+  );
+};
+export default TeamMemberCard;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
-import { Typography, Container } from '@mui/material';
+import { Typography, Container, Grid } from '@mui/material';
+import TeamMemberCard from '../components/TeamMemberCard';
+import {teamMembers} from "../constants/TeamMembers"
+import "../css/About.css";
+import hciLabLogo from "../res/HCI_Logo.png";
 
 const About = () => {
   return (
     <Container>
-      <Typography variant="h4" style={{  "text-align": "center"}}>What is Systematic Mobile Application Review?</Typography>
-      <Typography variant="body1">
+      <Typography variant="h4" className="about-header">What is Systematic Mobile Application Review?</Typography>
+      <Typography variant="body1" className="about-text">
         A tool for academic researchers to search mobile app store(s) to select a corpus of mobile apps that meet certain criteria (e.g., keywords in description, download count, app store rank). Researchers can then analyze data from this corpus of apps, such as app content (e.g., functionality, accessibility, language support) or metadata (e.g., permissions, user reviews, privacy labels). 
       </Typography>
+
+      <Typography variant="h4" className="about-header">SMAR Team</Typography>
+      <Grid container spacing={2} justifyContent="center">
+        {teamMembers.map((member, index) => (
+          <TeamMemberCard key={index} {...member} />
+        ))}
+      </Grid>
     </Container>
   );
 };
