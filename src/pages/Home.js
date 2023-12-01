21f7d06d85ab870a56d6c25d9b4785e48ac2532d
Akaash Trivedi <akaash.t1@gmail.com>
2023-11-30 23:48:53 -0800

    Home page css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import SearchBar from '../components/SearchBar';
 import Typography from '@mui/material/Typography';
+import "../css/Home.css";
 
 
 const Home = () => {
   return (
     <>
-        <div style={{  "text-align": "center"}}>
-          <Typography variant="h3" style={{ paddingTop: 20 }}>Systematic Mobile Application Review</Typography>
-          <Typography variant="p">A tool for academic researchers to conduct a keyword search of Google Play and get back the metadata for all relevant apps.</Typography>
+        <div className='home-container'>
+          <Typography variant="h3" className='home-header'>Systematic Mobile Application Review</Typography>
+          <Typography variant="p" className='home-text'>A tool for academic researchers to conduct a keyword search of Google Play and get back the metadata for all relevant apps.</Typography>
         </div>
         < SearchBar/>
     </>
