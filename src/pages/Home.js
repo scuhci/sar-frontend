079918d33c8628cfc8ca7d1dc97883c7a8dd0113
Akaash Trivedi <akaash.t1@gmail.com>
2024-05-13 01:23:35 -0700

    Add BETA version tag on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,13 +2,25 @@ import React from 'react';
 import SearchBar from '../components/SearchBar';
 import Typography from '@mui/material/Typography';
 import "../css/Home.css";
+import Chip from '@mui/joy/Chip';
 
 
 const Home = () => {
   return (
     <>
         <div className='home-container'>
-          <Typography variant="h3" className='home-header'>Systematic Mobile Application Review</Typography>
+          <Typography variant="h3" className='home-header'>
+            Systematic Mobile Application Reviews
+            <Chip
+              color="success"
+              onClick={function(){}}
+              size="sm"
+              variant="outlined"
+            >
+              BETA
+            </Chip>
+          </Typography>
+          
           <Typography variant="p" className='home-text'>A tool for academic researchers to conduct a keyword search of Google Play and get back the metadata for all relevant apps.</Typography>
         </div>
         < SearchBar/>
