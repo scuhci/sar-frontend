b454d15ed71be6aeed9986e2bf4bb73d00b4b35f
Jeshwin Prince <jeshwinjprince@gmail.com>
2024-08-13 15:21:43 -0700

    create mobile device banner UI, use dummy boolean for mobile device detection

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import SearchBar from '../components/SearchBar';
-import Typography from '@mui/material/Typography';
+import React from "react";
+import SearchBar from "../components/SearchBar";
+import MobileScreen from "../components/MobileScreen";
+import Typography from "@mui/material/Typography";
 import "../css/Home.css";
-import Chip from '@mui/joy/Chip';
-
+import Chip from "@mui/joy/Chip";
 
 const Home = () => {
-  return (
+  // Determine if the user is on a mobile device or a PC
+  // PLACEHOLDER! Change to actual mobile device detection
+  const isMobileDevice = true;
+
+  // Switch to mobile device screen if user is on a mobile device
+  return !isMobileDevice ? (
     <>
-        <div className='home-container'>
-          <Typography variant="h3" className='home-header'>
-            Systematic Mobile Application Reviews
-            <Chip
-              color="success"
-              onClick={function(){}}
-              size="sm"
-              variant="outlined"
-            >
-              BETA
-            </Chip>
-          </Typography>
-          
-          <Typography variant="p" className='home-text'>A tool for academic researchers to conduct a keyword search of Google Play and get back the metadata for all relevant apps.</Typography>
-        </div>
-        < SearchBar/>
+      <div className="home-container">
+        <Typography variant="h3" className="home-header">
+          Systematic Mobile Application Reviews
+          <Chip
+            color="success"
+            onClick={function () {}}
+            size="sm"
+            variant="outlined"
+          >
+            BETA
+          </Chip>
+        </Typography>
+
+        <Typography variant="p" className="home-text">
+          A tool for academic researchers to conduct a keyword search of Google
+          Play and get back the metadata for all relevant apps.
+        </Typography>
+      </div>
+      <SearchBar />
     </>
+  ) : (
+    <MobileScreen />
   );
 };
 
