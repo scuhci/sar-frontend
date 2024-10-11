02a8d66600b74b0bdaea6cf5dfa7749a7ee700ef
Lonalynn <lonavdlin@gmail.com>
2024-10-11 12:14:46 -0700

    Updated about page & added bio framework

--- src/components/TeamMemberCard.js
+++ src/components/TeamMemberCard.js
@@ -1,11 +1,46 @@
 import React from 'react';
-import { Card, CardContent, Typography, Avatar, Link, Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import { Card, CardContent, Typography, Avatar, Link, Grid, CardActions } from '@mui/material';
+import Collapse from '@mui/material/Collapse';
+import IconButton from '@mui/material/IconButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import "../css/TeamMemberCard.css"
 
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme }) => ({
+  marginLeft: 'auto',
+  transition: theme.transitions.create('transform', {
+    duration: theme.transitions.duration.shortest,
+  }),
+  variants: [
+    {
+      props: ({ expand }) => !expand,
+      style: {
+        transform: 'rotate(0deg)',
+      },
+    },
+    {
+      props: ({ expand }) => !!expand,
+      style: {
+        transform: 'rotate(180deg)',
+      },
+    },
+  ],
+}));
+
 const TeamMemberCard = ({ name, role, imageUrl, linkedInUrl }) => {
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+
   return (
     <Grid item xs={12} sm={4} md={4}>
-      <Card raised>
+      <Card raised   sx={{':hover': {boxShadow: 20,},}}>
         <CardContent className="team-card-content">
             <Avatar src={imageUrl} alt={name} className="team-avatar" style={{width: "70px", height: "70px"}}/>
             <div className="team-info">
@@ -16,6 +51,21 @@ const TeamMemberCard = ({ name, role, imageUrl, linkedInUrl }) => {
             </Link>
             </div>
         </CardContent>
+        <CardActions>
+          <ExpandMore
+            expand={expanded}
+            onClick={handleExpandClick}
+            aria-expanded={expanded}
+            aria-label="show more"
+          >
+            <ExpandMoreIcon />
+          </ExpandMore>
+        </CardActions>
+        <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          content
+        </CardContent>
+        </Collapse>
      </Card>
     </Grid>
   );
