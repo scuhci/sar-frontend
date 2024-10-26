a82cd31a9383556e64cc6a0130942891c9d1f1d9
Varun Mangla <Varunm57@outlook.com>
2024-10-25 21:38:17 -0700

    Converted basic CSV and relog to UTF-8. Need to convert the reviews CSV to UTF-8 and do some testing.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -189,7 +189,7 @@ const SearchBar = ({ flipState }) => {
       console.log(`Relog filename from header: ${filename_relog}`);
       // Create a URL from the blob
       const csv_file = new Blob(["\ufeff", response.data], { type: 'text/csv;charset=utf-8' });
-      const relog_file = new Blob(["\ufeff", relog_response.data], { type: 'text/csv;charset=utf-8' });
+      const relog_file = new Blob(["\ufeff", relog_response.data], { type: 'text/plain;charset=utf-8' });
       const zip = new JSZip();
       zip.file(filename, csv_file);
       zip.file(filename_relog, relog_file);
