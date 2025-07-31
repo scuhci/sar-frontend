8f0230bfbeac3079babc3c6e32e110fc6f2fbdd1
Raphael Kusuma <77198909+RaphaLK@users.noreply.github.com>
2025-07-31 10:01:30 -0700

    Proper email validation

--- src/components/NotifyMe.js
+++ src/components/NotifyMe.js
@@ -11,6 +11,9 @@ const NotifyMe = ({ country, query, permissions, queryTime }) => {
   //const pushQuery = "c:" + country + "_t:" + query + "_p:" + permissions + "_t:" + time; <-- match this in the backend
   const [email, setEmail] = useState("");
   const [invalidEmail, setInvalidEmail] = useState(false);
+  const [emailNotifySuccess, setEmailNotifySuccess] = useState(false);
+  const emailRegex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
   // in case perms is undefined
   const currentPermissions = permissions === true;
@@ -30,17 +33,20 @@ const NotifyMe = ({ country, query, permissions, queryTime }) => {
 
   const onNotifyClicked = async () => {
     try {
-      if (email === "" || !email.includes("@")) {
+      if (email === "" || !emailRegex.test(email)) {
         setInvalidEmail(true);
         return;
       } else {
         setInvalidEmail(false);
-        const response = await axios.post(`/email-notify?email=${email}&queryId=${pushQuery}`, {
-          email: email,
-          queryId: pushQuery, // unique ID for the specific query
-        });
-
-        console.log(response);
+        const response = await axios.post(
+          `/email-notify?email=${email}&queryId=${pushQuery}`,
+          {
+            email: email,
+            queryId: pushQuery, // unique ID for the specific query
+          }
+        );
+        setEmailNotifySuccess(true);
+        // console.log(response);
       }
     } catch (error) {
       console.log("Error trying to send Email to backend: ", error);
@@ -48,17 +54,31 @@ const NotifyMe = ({ country, query, permissions, queryTime }) => {
   };
   return (
     <div>
-      <div class="notificationWrapper">
-        <input
-          placeholder="Email Address"
-          class="emailField"
-          onChange={handleEmailInput}
-        ></input>
-        <button class="NotifyBox" onClick={onNotifyClicked}>
-          <span class="NotifyText">NOTIFY ME</span>
-        </button>
-      </div>
-      {invalidEmail && <p>Email is invalid, please try again</p>}
+      {!emailNotifySuccess && (
+        <div className="notificationWrapper">
+          <input
+            placeholder="Email Address"
+            className="emailField"
+            value={email}
+            onChange={handleEmailInput}
+          ></input>
+          <button className="NotifyBox" onClick={onNotifyClicked}>
+            <span className="NotifyText">NOTIFY ME</span>
+          </button>
+        </div>
+      )}
+      {!invalidEmail && emailNotifySuccess && (
+        <div className="notificationWrapper">
+          <p className="notificationSuccess">
+            Got it! We'll email you at{" "}
+            <span className="notificationSuccessEmail">{email}</span> as soon as
+            the results are ready.
+          </p>
+        </div>
+      )}
+      {invalidEmail && !emailNotifySuccess && (
+        <p>Email is invalid, please try again</p>
+      )}
     </div>
   );
 };
