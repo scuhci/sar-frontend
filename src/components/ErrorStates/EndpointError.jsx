f4145bcd573041171da259eb698635af4a466da3
RaphaLK <77198909+RaphaLK@users.noreply.github.com>
2026-04-05 14:36:34 -0700

    EndpointError Boxes

--- src/components/ErrorStates/EndpointError.jsx
+++ src/components/ErrorStates/EndpointError.jsx
@@ -1,59 +1,59 @@
 import React from "react";
 import "../../css/EndpointError.css";
 import { Typography, Box, Button } from "@mui/material";
-import "../../res/SMAR_Service_Error.png";
+import ServiceError from "../../res/SMAR_Service_Error.png";
+import ReloadButton from "./ReloadButton";
 
 const EndpointError = (endpointType, selectedScraper) => {
   return (
     <Box className="error-container">
+      <img src={ServiceError} alt="Service Error" />
       {endpointType === "GooglePlay" && (
         <Box>
           <Typography>Unable to reach Google Play Store</Typography>
-          <Button
-            className="search-button"
-            // onClick={}
-            variant="contained"
-            color="primary"
-          >
-            {selectedScraper !== "Play Store"
-              ? "Scrape Play Store"
-              : "Scrape App Store"}
-          </Button>
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Button
+              className="search-button"
+              // onClick={}
+              variant="contained"
+              color="primary"
+            >
+              {selectedScraper !== "Play Store"
+                ? "Scrape Play Store"
+                : "Scrape App Store"}
+            </Button>
+            <ReloadButton />
+          </Box>
         </Box>
       )}
       {endpointType === "AppStore" && (
         <Box>
           <Typography>Unable to reach App Store</Typography>
-          <Button
-            className="search-button"
-            // onClick={}
-            variant="contained"
-            color="primary"
-          >
-            {selectedScraper !== "Play Store"
-              ? "Scrape Play Store"
-              : "Scrape App Store"}
-          </Button>
+          <Box sx={{ display: "flex", flexDirection: "row" }}>
+            <Button
+              className="search-button"
+              // onClick={}
+              variant="contained"
+              color="primary"
+            >
+              {selectedScraper !== "Play Store"
+                ? "Scrape Play Store"
+                : "Scrape App Store"}
+            </Button>
+            <ReloadButton />
+          </Box>
         </Box>
       )}
       {endpointType === "TopCharts" && (
         <Box>
           <Typography>Unable to reach Top Charts</Typography>
-          <Button
-            className="search-button"
-            // onClick={}
-            variant="contained"
-            color="primary"
-          >
-            {selectedScraper !== "Play Store"
-              ? "Scrape Play Store"
-              : "Scrape App Store"}
-          </Button>
+          <ReloadButton />
         </Box>
       )}
       {endpointType === "BulkReviews" && (
         <Box>
           <Typography>Unable to reach Top Charts</Typography>
+          <ReloadButton />
         </Box>
       )}
     </Box>
